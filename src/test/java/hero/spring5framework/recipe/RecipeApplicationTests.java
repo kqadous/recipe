ef896a4a4fb9bc08facb6175f3cd61b3523ca98d
kqadous <60253971+kqadous@users.noreply.github.com>
2020-02-07 07:57:24 +0200

    Project initial commit

---
+++ src/test/java/hero/spring5framework/recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package hero.spring5framework.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
